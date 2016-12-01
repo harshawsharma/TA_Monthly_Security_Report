929c768bff7073df185317093b63b584ef322035
Sharma, Harsha Warrdhan <sharmhar@amazon.com>
2016-12-01 10:22:25 -0600

    Version 1

--- TA_Security_Checks.py
+++ TA_Security_Checks.py
@@ -1,18 +1,46 @@
 import boto3
 
-checks = ["nNauJisYIT", "c9D319e7sG", "Pfx0RwqBli", "vjafUGJ9H0", "N425c450f2",
-          "N430c450f2", "a2sEc6ILx", "xSqX82fQu", "12Fnkpl8Y5", "DqdJqYeRm5",
-          "Yw2K9puPzl", "zXCkfM1nI3", "7DAFEmoDos","HCP4007jGY", "1iG5NDGVre",]
+accountnumber= raw_input('Enter the Account Number: ')
+rolename= raw_input('Enter Role Name to assume: ')
+rolesession=accountnumber+rolename
 
-supportcli = boto3.client('support')
+# create an STS client object that represents a live connection to the STS service
+sts_client = boto3.client('sts')
 
-refresh = supportcli.describe_trusted_advisor_check_refresh_statuses(
-    checkIds = checks
+# Call the assume_role method of the STSConnection object and pass the role ARN and a role session name.
+assumedRoleObject = sts_client.assume_role(
+    RoleArn="arn:aws:iam::"+accountnumber+":role/"+rolename,
+    RoleSessionName=rolesession
 )
 
-for cids in checks:
-	results = supportcli.describe_trusted_advisor_check_result(
-		checkId = cids,
-		language ='en'
+# From the response that contains the assumed role, get the temporary credentials that can be used to make subsequent API calls
+credentials = assumedRoleObject['Credentials']
+
+# Create Support Client, region name is mandatory. This client is available only in us-east.
+# PASS the temp credentials of assumed role as arguments. From this point onwards these keys and thereby the
+# corresponding account will be used
+
+support = boto3.client('support',
+                       region_name='us-east-1',
+                       aws_access_key_id=credentials['AccessKeyId'],
+                       aws_secret_access_key=credentials['SecretAccessKey'],
+                       aws_session_token=credentials['SessionToken'],
+                       )
+
+# Obtain a list of all available checkids for Trusted Advisor.
+response_checkids = support.describe_trusted_advisor_checks(
+    language='en'
+)
+
+#Go through the dictionary list and extract the checkids only for the security category
+for listobjects in response_checkids['checks']:
+    if listobjects['category'] == 'security':
+        extracted_checkids = listobjects['id']
+
+    #Only for the extracted checkids refresh the current status and print it
+        response_refreshstatus = support.describe_trusted_advisor_check_refresh_statuses(
+            checkIds=[
+                extracted_checkids
+            ]
         )
-	print results
+        print response_refreshstatus
